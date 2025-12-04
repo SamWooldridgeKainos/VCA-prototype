aab84e0baa9b3bcb0d283c0af6da5b082eafa2a4
Sam Wooldridge <s.wooldridge@kainos.com>
2025-12-04 02:00:13 +0000

    Merge nightmare fix!

--- app/routes.js
+++ app/routes.js
@@ -218,25 +218,20 @@ router.post('/ur/nfa/start-3/sign-in-answer', function(request, response) {
 })
 
 router.post('/ur/nfa/call/telephone-call-1-details-answer', function(request, response) {
-router.post('/ur/nfa/call/telephone-call-1-details-answer', function(request, response) {
 
     var victimInformed = request.session.data['victimInformed']
     if (victimInformed == "Yes"){
         response.redirect("/ur/nfa/call/follow-up-moc?callAttempt=1")
-        response.redirect("/ur/nfa/call/follow-up-moc?callAttempt=1")
     } else {
-        response.redirect("/ur/nfa/call/text-message-details?callAttempt=1")
         response.redirect("/ur/nfa/call/text-message-details?callAttempt=1")
     }
 })
 
 router.post('/ur/nfa/call/text-message-details-answer', function(request, response) {
-router.post('/ur/nfa/call/text-message-details-answer', function(request, response) {
 
     response.redirect("/ur/nfa/victim-record?pmoc=mobile&nfaStatus=after-call-attempt-1#communications")
 })
 
-router.post('/ur/nfa/call/next-attempt-moc-answer', function(request, response) {
 router.post('/ur/nfa/call/next-attempt-moc-answer', function(request, response) {
 
     var attemptToContactAgain = request.session.data['attemptToContactAgain']
@@ -245,9 +240,7 @@ router.post('/ur/nfa/call/next-attempt-moc-answer', function(request, response) 
     if (attemptToContactAgain == "Call") {
         if (callAttempt == "1") {
             response.redirect("/ur/nfa/call/telephone-call-2-details")
-            response.redirect("/ur/nfa/call/telephone-call-2-details")
         } else {
-            response.redirect("/ur/nfa/call/telephone-call-3-details")
             response.redirect("/ur/nfa/call/telephone-call-3-details")
         }
     } else if (attemptToContactAgain == "Email") {
@@ -259,18 +252,15 @@ router.post('/ur/nfa/call/next-attempt-moc-answer', function(request, response) 
 })
 
 router.post('/ur/nfa/call/telephone-call-2-details-answer', function(request, response) {
-router.post('/ur/nfa/call/telephone-call-2-details-answer', function(request, response) {
 
     var victimInformed = request.session.data['victimInformed']
     if (victimInformed == "Yes"){
-        response.redirect("/ur/nfa/call/follow-up-moc?callAttempt=2")
         response.redirect("/ur/nfa/call/follow-up-moc?callAttempt=2")
     } else {
         response.redirect("/ur/nfa/victim-record?nfaStatus=after-call-attempt-2&callAttempt=2#communications")
     }
 })
 
-router.post('/ur/nfa/call/follow-up-moc-answer', function(request, response) {
 router.post('/ur/nfa/call/follow-up-moc-answer', function(request, response) {
 
     var fumoc = request.session.data['fumoc']
@@ -304,11 +294,9 @@ router.post('/ur/nfa/call/follow-up-moc-answer', function(request, response) {
 })
 
 router.post('/ur/nfa/call/telephone-call-3-details-answer', function(request, response) {
-router.post('/ur/nfa/call/telephone-call-3-details-answer', function(request, response) {
 
     var victimInformed = request.session.data['victimInformed']
     if (victimInformed == "Yes"){
-        response.redirect("/ur/nfa/call/follow-up-moc?callAttempt=3")
         response.redirect("/ur/nfa/call/follow-up-moc?callAttempt=3")
     } else {
         response.redirect("/ur/nfa/victim-record?nfaStatus=after-call-attempt-3&callAttempt=3#communications")
@@ -405,24 +393,19 @@ router.post('/delivery/nfa/draft/cd-modal/request-review-answer', function(reque
 })
 
 router.post('/delivery/nfa/call/telephone-call-1-details-answer', function(request, response) {
-router.post('/delivery/nfa/call/telephone-call-1-details-answer', function(request, response) {
 
     var victimInformed1 = request.session.data['victimInformed1']
     if (victimInformed1 == "Yes"){
         response.redirect("/delivery/nfa/call/follow-up-moc?callAttempt=1&success=yes&successReason=informed-after-call-1")
-        response.redirect("/delivery/nfa/call/follow-up-moc?callAttempt=1&success=yes&successReason=informed-after-call-1")
     } else {
-        response.redirect("/delivery/nfa/call/was-text-message-sent?callAttempt=1&success=yes&successReason=not-informed-after-call-1")
         response.redirect("/delivery/nfa/call/was-text-message-sent?callAttempt=1&success=yes&successReason=not-informed-after-call-1")
     }
 })
 
 router.post('/delivery/nfa/call/was-text-message-sent-answer', function(request, response) {
-router.post('/delivery/nfa/call/was-text-message-sent-answer', function(request, response) {
 
     var wasTextMessageSent = request.session.data['wasTextMessageSent']
     if (wasTextMessageSent == "Yes"){
-        response.redirect("/delivery/nfa/call/text-message-details")
         response.redirect("/delivery/nfa/call/text-message-details")
     } else {
         response.redirect("/delivery/nfa/victim-record?pmoc=mobile&nfaStatus=after-call-attempt-1#communications")
@@ -430,12 +413,10 @@ router.post('/delivery/nfa/call/was-text-message-sent-answer', function(request,
 })
 
 router.post('/delivery/nfa/call/text-message-details-answer', function(request, response) {
-router.post('/delivery/nfa/call/text-message-details-answer', function(request, response) {
 
     response.redirect("/delivery/nfa/victim-record?nfaStatus=after-call-attempt-1#communications")
 })
 
-router.post('/delivery/nfa/call/next-attempt-moc-answer', function(request, response) {
 router.post('/delivery/nfa/call/next-attempt-moc-answer', function(request, response) {
 
     var attemptToContactAgain = request.session.data['attemptToContactAgain']
@@ -444,9 +425,7 @@ router.post('/delivery/nfa/call/next-attempt-moc-answer', function(request, resp
     if (attemptToContactAgain == "Call") {
         if (callAttempt == "1") {
             response.redirect("/delivery/nfa/call/telephone-call-2-details")
-            response.redirect("/delivery/nfa/call/telephone-call-2-details")
         } else {
-            response.redirect("/delivery/nfa/call/telephone-call-3-details")
             response.redirect("/delivery/nfa/call/telephone-call-3-details")
         }
     } else if (attemptToContactAgain == "Email") {
@@ -465,18 +444,15 @@ router.post('/delivery/nfa/call/next-attempt-moc-answer', function(request, resp
 })
 
 router.post('/delivery/nfa/call/telephone-call-2-details-answer', function(request, response) {
-router.post('/delivery/nfa/call/telephone-call-2-details-answer', function(request, response) {
 
     var victimInformed2 = request.session.data['victimInformed2']
     if (victimInformed2 == "Yes"){
-        response.redirect("/delivery/nfa/call/follow-up-moc?callAttempt=2&success=yes&successReason=informed-after-call-2")
         response.redirect("/delivery/nfa/call/follow-up-moc?callAttempt=2&success=yes&successReason=informed-after-call-2")
     } else {
         response.redirect("/delivery/nfa/victim-record?nfaStatus=after-call-attempt-2&callAttempt=2&success=yes&successReason=not-informed-after-call-2#communications")
     }
 })
 
-router.post('/delivery/nfa/call/follow-up-moc-answer', function(request, response) {
 router.post('/delivery/nfa/call/follow-up-moc-answer', function(request, response) {
 
     var fumoc = request.session.data['fumoc']
@@ -510,11 +486,9 @@ router.post('/delivery/nfa/call/follow-up-moc-answer', function(request, respons
 })
 
 router.post('/delivery/nfa/call/telephone-call-3-details-answer', function(request, response) {
-router.post('/delivery/nfa/call/telephone-call-3-details-answer', function(request, response) {
 
     var victimInformed3 = request.session.data['victimInformed3']
     if (victimInformed3 == "Yes"){
-        response.redirect("/delivery/nfa/call/follow-up-moc?callAttempt=3&success=yes&successReason=informed-after-call-3")
         response.redirect("/delivery/nfa/call/follow-up-moc?callAttempt=3&success=yes&successReason=informed-after-call-3")
     } else {
         response.redirect("/delivery/nfa/victim-record?nfaStatus=after-call-attempt-3&callAttempt=3&success=yes&successReason=not-informed-after-call-3#communications")
@@ -604,24 +578,19 @@ router.post('/ur/vcl/draft/cd-modal/request-review-answer', function(request, re
 })
 
 router.post('/ur/vcl/call/phone-call-1-answer', function(request, response) {
-router.post('/ur/vcl/call/phone-call-1-answer', function(request, response) {
 
     var victimInformed1 = request.session.data['victimInformed1']
     if (victimInformed1 == "Yes"){
         response.redirect("/ur/vcl/call/follow-up-moc?callAttempt=1&success=yes&successReason=informed-after-call-1")
-        response.redirect("/ur/vcl/call/follow-up-moc?callAttempt=1&success=yes&successReason=informed-after-call-1")
     } else {
-        response.redirect("/ur/vcl/call/was-text-message-sent?callAttempt=1&success=yes&successReason=not-informed-after-call-1")
         response.redirect("/ur/vcl/call/was-text-message-sent?callAttempt=1&success=yes&successReason=not-informed-after-call-1")
     }
 })
 
 router.post('/ur/vcl/call/was-text-message-sent-answer', function(request, response) {
-router.post('/ur/vcl/call/was-text-message-sent-answer', function(request, response) {
 
     var wasTextMessageSent = request.session.data['wasTextMessageSent']
     if (wasTextMessageSent == "Yes"){
-        response.redirect("/ur/vcl/call/text-message-details")
         response.redirect("/ur/vcl/call/text-message-details")
     } else {
         response.redirect("/ur/vcl/victim-record?pmoc=mobile&vclStatus=after-call-attempt-1&subTab=vcl#communications")
@@ -629,12 +598,10 @@ router.post('/ur/vcl/call/was-text-message-sent-answer', function(request, respo
 })
 
 router.post('/ur/vcl/call/text-message-details-answer', function(request, response) {
-router.post('/ur/vcl/call/text-message-details-answer', function(request, response) {
 
     response.redirect("/ur/vcl/victim-record?vclStatus=after-call-attempt-1&subTab=vcl#communications")
 })
 
-router.post('/ur/vcl/call/next-attempt-moc-answer', function(request, response) {
 router.post('/ur/vcl/call/next-attempt-moc-answer', function(request, response) {
 
     var attemptToContactAgain = request.session.data['attemptToContactAgain']
@@ -643,9 +610,7 @@ router.post('/ur/vcl/call/next-attempt-moc-answer', function(request, response) 
     if (attemptToContactAgain == "Call") {
         if (callAttempt == "1") {
             response.redirect("/ur/vcl/call/phone-call-2")
-            response.redirect("/ur/vcl/call/phone-call-2")
         } else {
-            response.redirect("/ur/vcl/call/phone-call-3")
             response.redirect("/ur/vcl/call/phone-call-3")
         }
     } else if (attemptToContactAgain == "Email") {
@@ -664,18 +629,15 @@ router.post('/ur/vcl/call/next-attempt-moc-answer', function(request, response) 
 })
 
 router.post('/ur/vcl/call/phone-call-2-answer', function(request, response) {
-router.post('/ur/vcl/call/phone-call-2-answer', function(request, response) {
 
     var victimInformed2 = request.session.data['victimInformed2']
     if (victimInformed2 == "Yes"){
-        response.redirect("/ur/vcl/call/follow-up-moc?callAttempt=2&success=yes&successReason=informed-after-call-2")
         response.redirect("/ur/vcl/call/follow-up-moc?callAttempt=2&success=yes&successReason=informed-after-call-2")
     } else {
         response.redirect("/ur/vcl/victim-record?vclStatus=after-call-attempt-2&callAttempt=2&success=yes&successReason=not-informed-after-call-2&subTab=vcl#communications")
     }
 })
 
-router.post('/ur/vcl/call/follow-up-moc-answer', function(request, response) {
 router.post('/ur/vcl/call/follow-up-moc-answer', function(request, response) {
 
     var fumoc = request.session.data['fumoc']
@@ -709,11 +671,9 @@ router.post('/ur/vcl/call/follow-up-moc-answer', function(request, response) {
 })
 
 router.post('/ur/vcl/call/phone-call-3-answer', function(request, response) {
-router.post('/ur/vcl/call/phone-call-3-answer', function(request, response) {
 
     var victimInformed3 = request.session.data['victimInformed3']
     if (victimInformed3 == "Yes"){
-        response.redirect("/ur/vcl/call/follow-up-moc?callAttempt=3&success=yes&successReason=informed-after-call-3")
         response.redirect("/ur/vcl/call/follow-up-moc?callAttempt=3&success=yes&successReason=informed-after-call-3")
     } else {
         response.redirect("/ur/vcl/victim-record?vclStatus=after-call-attempt-3&callAttempt=3&success=yes&successReason=not-informed-after-call-3&subTab=vcl#communications")
@@ -799,24 +759,19 @@ router.post('/delivery/vcl/draft/cd-modal/request-review-answer', function(reque
 })
 
 router.post('/delivery/vcl/call/phone-call-1-answer', function(request, response) {
-router.post('/delivery/vcl/call/phone-call-1-answer', function(request, response) {
 
     var victimInformed1 = request.session.data['victimInformed1']
     if (victimInformed1 == "Yes"){
         response.redirect("/delivery/vcl/call/follow-up-moc?callAttempt=1&success=yes&successReason=informed-after-call-1")
-        response.redirect("/delivery/vcl/call/follow-up-moc?callAttempt=1&success=yes&successReason=informed-after-call-1")
     } else {
-        response.redirect("/delivery/vcl/call/was-text-message-sent?callAttempt=1&success=yes&successReason=not-informed-after-call-1")
         response.redirect("/delivery/vcl/call/was-text-message-sent?callAttempt=1&success=yes&successReason=not-informed-after-call-1")
     }
 })
 
 router.post('/delivery/vcl/call/was-text-message-sent-answer', function(request, response) {
-router.post('/delivery/vcl/call/was-text-message-sent-answer', function(request, response) {
 
     var wasTextMessageSent = request.session.data['wasTextMessageSent']
     if (wasTextMessageSent == "Yes"){
-        response.redirect("/delivery/vcl/call/text-message-details")
         response.redirect("/delivery/vcl/call/text-message-details")
     } else {
         response.redirect("/delivery/vcl/victim-record?pmoc=mobile&vclStatus=after-call-attempt-1&subTab=vcl#communications")
@@ -824,27 +779,21 @@ router.post('/delivery/vcl/call/was-text-message-sent-answer', function(request,
 })
 
 router.post('/delivery/vcl/call/text-message-details-answer', function(request, response) {
-router.post('/delivery/vcl/call/text-message-details-answer', function(request, response) {
 
     response.redirect("/delivery/vcl/victim-record?vclStatus=after-call-attempt-1&subTab=vcl#communications")
 })
 
-router.post('/delivery/vcl/call/next-attempt-moc-answer', function(request, response) {
 router.post('/delivery/vcl/call/next-attempt-moc-answer', function(request, response) {
 
     var attemptToContactAgain = request.session.data['attemptToContactAgain']
     var callAttempt = request.session.data['callAttempt']
 
     if (attemptToContactAgain == "call") {
-    if (attemptToContactAgain == "call") {
         if (callAttempt == "1") {
-            response.redirect("/delivery/vcl/call/phone-call-2")
             response.redirect("/delivery/vcl/call/phone-call-2")
         } else {
             response.redirect("/delivery/vcl/call/phone-call-3")
-            response.redirect("/delivery/vcl/call/phone-call-3")
         }
-    } else if (attemptToContactAgain == "email") {
     } else if (attemptToContactAgain == "email") {
         if (callAttempt == "1") {
             response.redirect("/delivery/vcl/victim-record?vclStatus=draft-ready-to-send&attemptToContactAgain=email&subTab=vcl#communications")
@@ -861,18 +810,15 @@ router.post('/delivery/vcl/call/next-attempt-moc-answer', function(request, resp
 })
 
 router.post('/delivery/vcl/call/phone-call-2-answer', function(request, response) {
-router.post('/delivery/vcl/call/phone-call-2-answer', function(request, response) {
 
     var victimInformed2 = request.session.data['victimInformed2']
     if (victimInformed2 == "Yes"){
-        response.redirect("/delivery/vcl/call/follow-up-moc?callAttempt=2&success=yes&successReason=informed-after-call-2")
         response.redirect("/delivery/vcl/call/follow-up-moc?callAttempt=2&success=yes&successReason=informed-after-call-2")
     } else {
         response.redirect("/delivery/vcl/victim-record?vclStatus=after-call-attempt-2&callAttempt=2&success=yes&successReason=not-informed-after-call-2&subTab=vcl#communications")
     }
 })
 
-router.post('/delivery/vcl/call/follow-up-moc-answer', function(request, response) {
 router.post('/delivery/vcl/call/follow-up-moc-answer', function(request, response) {
 
     var fumoc = request.session.data['fumoc']
@@ -906,11 +852,9 @@ router.post('/delivery/vcl/call/follow-up-moc-answer', function(request, respons
 })
 
 router.post('/delivery/vcl/call/phone-call-3-answer', function(request, response) {
-router.post('/delivery/vcl/call/phone-call-3-answer', function(request, response) {
 
     var victimInformed3 = request.session.data['victimInformed3']
     if (victimInformed3 == "Yes"){
-        response.redirect("/delivery/vcl/call/follow-up-moc?callAttempt=3&success=yes&successReason=informed-after-call-3")
         response.redirect("/delivery/vcl/call/follow-up-moc?callAttempt=3&success=yes&successReason=informed-after-call-3")
     } else {
         response.redirect("/delivery/vcl/victim-record?vclStatus=after-call-attempt-3&callAttempt=3&success=yes&successReason=not-informed-after-call-3&subTab=vcl#communications")
