8c54c5c5f1396cfb06e7a6adf448dd1cb1a36ce8
Michael Ashinze <34244307+mikeash82@users.noreply.github.com>
2026-06-03 19:58:08 +0100

    updates (#220)

--- app/routes/delivery/wat4.js
+++ app/routes/delivery/wat4.js
@@ -295,6 +295,12 @@ module.exports = router => {
         response.redirect("/delivery/wat4/onb/check-task?manualTask=no")
     })
 
+    
+    router.post('/ur/bfs/onb/new-task/next-task-due-date-answer', function(request, response) {
+
+        response.redirect("/ur/bfs/onb/new-task/check-task?manualTask=no")
+    })
+
     router.post('/delivery/wat4/onb/manual-task-answer', function(request, response) {
 
         response.redirect("/delivery/wat4/onb/check-task?manualTask=yes")
@@ -352,9 +358,39 @@ module.exports = router => {
         }
     })
 
+
+
+    router.post('/ur/bfs/onb/next-task-answer', function(request, response) {
+
+        var nextTask = request.session.data['nextTask']
+
+        if (nextTask == "dtc") {
+            response.redirect("/delivery/wat4/victim/new-task/task-due-date?pcdType=dtc")
+        } else if (nextTask == "nfa") {
+            response.redirect("/delivery/wat4/victim/new-task/task-due-date?pcdType=nfa")
+        } else if (nextTask == "stopped-charge") {
+            response.redirect("/delivery/wat4/victim/new-task/task-due-date?vclType=stopped-charge")
+        } else if (nextTask == "altered-charge") {
+            response.redirect("/delivery/wat4/victim/new-task/task-due-date?vclType=altered-charge")
+        } else if (nextTask == "other") {
+            response.redirect("/delivery/wat4/victim/new-task/manual-task")
+        } else if (nextTask == "no-task") {
+            response.redirect("/delivery/wat4/victim/new-task/check-task")
+        } else if (nextTask == "meeting-offer" || nextTask == "meeting-arranged" || nextTask == "meeting-outcome") {
+            response.redirect("/ur/bfs/onb/new-task/meeting-purpose")
+        } else {
+            response.redirect("/delivery/wat4/victim/new-task/task-due-date")
+        }
+    })
+
     router.post('/delivery/wat4/victim/new-task/meeting-purpose-answer', function(request, response) {
 
         response.redirect("/delivery/wat4/victim/new-task/task-due-date")
+    })
+
+       router.post('/ur/bfs/onb/new-task/meeting-purpose-answer', function(request, response) {
+
+        response.redirect("/ur/bfs/onb/new-task/next-task-due-date")
     })
 
     router.get('/delivery/wat4/victim/new-task/task-due-date', function(request, response) {
@@ -365,9 +401,16 @@ module.exports = router => {
         })
     })
 
+    
     router.post('/delivery/wat4/victim/new-task/task-due-date-answer', function(request, response) {
 
         response.redirect("/delivery/wat4/victim/new-task/check-task")
+    })
+
+
+      router.post('/ur/bfs/onb/new-task/task-due-date-answer', function(request, response) {
+
+        response.redirect("/ur/bfs/onb/new-task/check-task")
     })
 
     router.post('/delivery/wat4/victim/new-task/manual-task-answer', function(request, response) {
@@ -382,6 +425,15 @@ module.exports = router => {
         request.session.data['existingMeetingPurpose'] = request.session.data['meetingPurpose'] || ''
 
         response.redirect("/delivery/wat4/victim/new-task/task-created")
+    })
+
+      router.post('/ur/bfs/onb/new-task/check-task-answer', function(request, response) {
+
+        // Update existing task tracking when a task is confirmed
+        request.session.data['existingTask'] = request.session.data['nextTask'] || ''
+        request.session.data['existingMeetingPurpose'] = request.session.data['meetingPurpose'] || ''
+
+        response.redirect("/ur/bfs/onb/new-task/task-created")
     })
 
     router.post('/delivery/wat4/victim/new-task/task-created-answer', function(request, response) {
@@ -953,6 +1005,11 @@ module.exports = router => {
   router.post('/delivery/wat4/wft-meetings/new-task/task-due-date-answer', function(request, response) {
 
         response.redirect("/delivery/wat4/wft-meetings/new-task/check-task")
+    })
+
+     router.post('/ur/bfs/onb/new-task/task-due-date-answer', function(request, response) {
+
+        response.redirect("/ur/bfs/onb/new-task/new-task/check-task")
     })
 
 
